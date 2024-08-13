c4fe1445e9cb98e15725642097d696d08e8de60d
Felipe Goyes <pipegoyes@gmail.com>
2024-08-13 15:32:31 +0200

    Basic eskeleton with primeng and its icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainInformationComponent } from './main-information/main-information.component';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api/menuitem';
+import { MenuModule } from 'primeng/menu';
+import { CardModule } from 'primeng/card';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainInformationComponent, SidebarModule, ButtonModule, MenuModule, CardModule, AvatarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cv-viewer';
+  sidebarVisible: boolean = false;
+  expirenceItems: MenuItem[] | undefined;
+  contactItems: MenuItem[] | undefined;
+
+  constructor(){
+    this.expirenceItems = [
+      {
+          label: 'Expirence',
+          items: [
+              {
+                  label: 'Projects',
+                  icon: 'pi pi-plus'
+              },
+              {
+                  label: 'Technologies',
+                  icon: 'pi pi-search'
+              }
+          ]
+      }
+    ]
+
+    this.contactItems = [
+      { label: 'Mainz / Frankfurt', icon: 'pi pi-home' },
+      { label: 'f.goyes@bojsen.eu', icon: 'pi pi-envelope' },
+      { label: 'Mobile: +49 163 249 5024', icon: 'pi pi-phone' }
+  ];
+  }
+
 }
---
+++ src/app/main-information/Employee.ts
@@ -0,0 +1,4 @@
+
+export declare interface Employee {
+  name: string;
+}
