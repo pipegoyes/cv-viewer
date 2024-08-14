77ce7b68c8db69aa022baf87588ed1d2acc552a6
Felipe Goyes <pipegoyes@gmail.com>
2024-08-14 14:55:06 +0200

    Attach projects json into compoment

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { PersonService } from '../person.service';
+import { Project } from '../../domain/person';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CardModule],
+  imports: [CardModule, CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+  projects: Project[] | undefined;
+  constructor(private personService: PersonService){
 
+  }
+
+  ngOnInit(){
+    this.personService.getPerson("felipe", "EN").subscribe(data => {
+      this.projects = data.projects;
+    })
+  }
 }
